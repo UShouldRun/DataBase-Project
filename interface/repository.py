ac6072098af5fef3db6e860e237c93be858331a4
Alexandre Furriel <up202307781@up.pt>
2024-12-03 17:41:27 +0000

    v.0.5.4

--- interface/repository.py
+++ interface/repository.py
@@ -9,18 +9,18 @@ def call_show_all_actors() -> list[str]:
     return actors_array
 
 
-def call_show_genre(title: string) -> Table:
+def call_show_genre(title: str) -> Table:
     return db.session.execute(
         text("""CALL show_genre(:title)"""), { 'title': title }
     )
     return table
 
-def call_show_streaming_countries(title: string) -> Table:
+def call_show_streaming_countries(title: str) -> Table:
     return db.session.execute(
         text("""CALL show_streaming_countries(:title)"""), { 'title': title }
     )
 
-def call_show_directors(title: string) -> Table:
+def call_show_directors(title: str) -> Table:
     return db.session.execute(
         text("""CALL show_directors(:title)"""), { 'title': title }
     )
@@ -32,7 +32,7 @@ def call_show_actors():
     actors_array = [dict(row) for row in result.fetchall()]
     return actors_array
 
-def call_show_top10_genre(category_type: string) -> Table:
+def call_show_top10_genre(category_type: str) -> Table:
    return db.session.execute(
         text("""CALL show_yearly_count(:category_type)"""), { 'category_type': category_type }
     ) 
