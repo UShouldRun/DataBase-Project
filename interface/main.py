ed4d5023e15f768c3b326068379267959dc13ad7
João Pedro Ferreira <jotapgferreira@hotmail.com>
2024-12-03 00:00:56 +0000

    v0.5.0 Frontend implementation (not complete)

--- interface/main.py
+++ interface/main.py
@@ -2,7 +2,7 @@ from config import app, initialize_database, populate_database
 from views import *
 
 if __name__=="__main__":
-    
-    initialize_database()
-    populate_database()
+    # use these two functions when you first initialize the database and then its not necessary anymore 
+    #initialize_database()
+    #populate_database()
     app.run(debug=True)
--- interface/views.py
+++ interface/views.py
@@ -4,8 +4,44 @@ from flask import render_template
 # routes
 @app.route("/")
 def homepage():
-    return render_template("homepage.html", author="João")
+    return render_template("homepage.html")
 
-@app.route("/fpage")
-def fpage():
-    return "This is the first page"
+@app.route("/actors")
+def show_all_actors():
+    actors = [
+        {"ActorId": 1, "Name": "João"},
+        {"ActorId": 2, "Name": "Rossi"},
+        {"ActorId": 3, "Name": "Alex"},
+        {"ActorId": 4, "Name": "Henrique"}
+    ]
+    return render_template("actors.html", actors=actors)
+
+@app.route("/shows")
+def show_all_shows():
+    shows = [
+    {
+        "type": "Movie",
+        "title": "As aventuras da cadeira de base de dados",
+        "director": ["João", "Henrique"],
+        "cast": ["João", "Henrique", "Rossi", "Alex"],
+        "country": ["Portugal"],
+        "release_year": [2024],
+        "rating": "TOP G",
+        "duration": "1 mês",
+        "listed_in": ["Comedy", "Dramatic"],
+        "description": ["4 jovens tentam desesperadamente acabar uma app para a faculdade"],
+    },
+    {
+        "type": "Movie",
+        "title": "O comuna tirano",
+        "director": ["Rogério"],
+        "cast": ["Rogério"],
+        "country": ["Portugal"],
+        "release_year": [2024],
+        "rating": "Salvem me",
+        "duration": "2 semestres",
+        "listed_in": ["Comedy", "Dramatic","Terror"],
+        "description": ["O comuna tirano dá-nos aulas de estruturas de dados e modelos de computação"],
+    }
+    ]
+    return render_template("shows.html", shows=shows)
